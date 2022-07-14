6875319d1075a5f3234c2003679cd7ff098e5d24
cvvergara <vicky@georepublic.de>
2022-07-13 21:51:00 -0500

    adding the presentation css

--- presentation/conf.py
+++ presentation/conf.py
@@ -32,7 +32,7 @@ html_theme_options = {
 # -- Options for Reveal.js output ---------------------------------------------
 revealjs_static_path = ["'@CMAKE_CURRENT_SOURCE_DIR@/../_static'"]
 revealjs_google_fonts = ["M PLUS 1p",]
-revealjs_style_theme = "white"
+revealjs_style_theme = "presentation.css"
 revealjs_script_conf = """
     {
         controls: true,
